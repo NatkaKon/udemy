6a91d3d9f31f0917540613b3b62a53b56ceb75d8
Natalia Kondratieva <kondratieva07@gmail.com>
2022-05-16 21:17:54 +0300

    feat: done section 2 task 19

--- script.js
+++ script.js
@@ -22,4 +22,27 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+let a = prompt('Один из последних просмотренных фильмов?', '');
+let b = +prompt('На сколько оцените его?', '');
+
+let c = prompt('Один из последних просмотренных фильмов?', '');
+let d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
+
+
 
